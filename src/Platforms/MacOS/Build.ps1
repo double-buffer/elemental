86b4a5688c8bdaac03a5d84a5c201afa0d24ede2
Thomas Decroyère <thomas.decroyere@outlook.com>
2023-01-20 11:34:12 +0100

    Config changes

--- src/Platforms/MacOS/Build.ps1
+++ src/Platforms/MacOS/Build.ps1
@@ -13,8 +13,8 @@ try
     $currentDirectory = (Get-Item -Path .).BaseName
     Write-Output $currentDirectory
 
-    if (-not($currentDirectory -eq "Windows")) {
-        Push-Location ./../Platforms/Windows
+    if (-not($currentDirectory -eq "MacOS")) {
+        Push-Location ./../Platforms/MacOS
     }
 
     mkdir obj | Out-Null
@@ -24,7 +24,8 @@ try
         Write-Output "Debug"
         swiftc *.swift -wmo -emit-library -module-name "ElementalPlatformNative" -v -Onone -g -o "obj/Debug/Elemental.Native.dylib" -I "." -debug-info-format=dwarf -swift-version 5 -target x86_64-apple-macosx13 -Xlinker -rpath -Xlinker "@executable_path/../Frameworks"
     } else {
-        swiftc *.swift -wmo -emit-library -module-name "ElementalPlatformNative" -O -g -o "obj/Release/Elemental.Native.dylib" -I "." -debug-info-format=dwarf -swift-version 5 -target x86_64-apple-macosx13 -Xlinker -rpath -Xlinker "@executable_path/../Frameworks"
+        Write-Output "Release"
+        swiftc *.swift -wmo -emit-library -module-name "ElementalPlatformNative" -v -O -g -o "obj/Release/Elemental.Native.dylib" -I "." -debug-info-format=dwarf -swift-version 5 -target x86_64-apple-macosx13 -Xlinker -rpath -Xlinker "@executable_path/../Frameworks"
     }
 
     if (-Not $?) {
