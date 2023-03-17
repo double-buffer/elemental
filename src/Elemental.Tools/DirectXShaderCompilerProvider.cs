f7ecf44729589e6563574a6fd8b82daac9c71250
Thomas Decroyère <thomas.decroyere@outlook.com>
2023-03-17 15:46:36 +0100

    Fix dxc

--- src/Elemental.Tools/DirectXShaderCompilerProvider.cs
+++ src/Elemental.Tools/DirectXShaderCompilerProvider.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 namespace Elemental.Tools;
@@ -32,16 +33,11 @@ internal class DirectXShaderCompilerProvider : IShaderCompilerProvider
 
     public unsafe ShaderCompilerResult CompileShader(ReadOnlySpan<byte> shaderCode, ToolsShaderStage shaderStage, string entryPoint, ShaderLanguage shaderLanguage, ToolsGraphicsApi graphicsApi)
     {
-        if (_dxilLibraryPath != null)
-        {
-            NativeLibrary.Load(_dxilLibraryPath);
-        }
+        var inputFilePath = $"{Path.GetTempFileName()}.hlsl";
+        var outputFilePath = Path.GetTempFileName();
 
-        if (!DirectXShaderCompilerInterop.DxcCreateInstance(DirectXShaderCompilerInterop.CLSID_DxcCompiler, out IDxcCompiler compiler))
-        {
-            return ShaderCompilerResult.CreateErrorResult("Cannot instantiate DirectX Shader Compiler.");
-        }
-
+        File.WriteAllBytes(inputFilePath, shaderCode.ToArray());
+        
         var arguments = new List<string>()
         {
             "-HV 2021"
@@ -68,152 +64,55 @@ internal class DirectXShaderCompilerProvider : IShaderCompilerProvider
             shaderTarget = "ps_6_7";
         }
 
-        var compileResult = compiler.Compile(new BinaryBlob(shaderCode), "SourceShader.hlsl", entryPoint, shaderTarget, arguments.ToArray(), arguments.Count, null, 0, null);
+        using var process = Process.Start(new ProcessStartInfo 
+        { 
+            FileName = "ShaderCompilers/dxc", 
+            Arguments = $"-T {shaderTarget} -E {entryPoint} -Fo {outputFilePath} {string.Join(' ', arguments.ToArray())} {inputFilePath}", 
+            RedirectStandardError = true, 
+            RedirectStandardOutput = true
+        });
 
-        var errors = compileResult.GetErrors();
-        var bufferPointer = errors.GetBufferPointer();
-        var outputString = Utf8StringMarshaller.ConvertToManaged((byte*)bufferPointer);
+        if (process == null)
+        {
+            return ShaderCompilerResult.CreateErrorResult("Cannot start Metal shader compiler process.");
+        }
 
-        var dataResult = compileResult.GetResult();
-        var shaderData = new Span<byte>(dataResult.GetBufferPointer(), (int)dataResult.GetBufferSize());
-
+        process.WaitForExit();
+        string? line;
         var logList = new List<ShaderCompilerLogEntry>();
         var currentLogType = ShaderCompilerLogEntryType.Error;
         var hasErrors = false;
 
-        if (outputString != null)
+        while ((line = process.StandardError.ReadLine()) != null)
         {
-            foreach (var line in outputString.Split('\n'))
+            if (line.Contains("warning:"))
             {
-                if (string.IsNullOrEmpty(line))
-                {
-                    continue;
-                }
-
-                if (line.Contains("warning:"))
-                {
-                    currentLogType = ShaderCompilerLogEntryType.Warning;
-                }
-                else if (line.Contains("error:"))
-                {
-                    currentLogType = ShaderCompilerLogEntryType.Error;
-                    hasErrors = true;
-                }
-
-                logList.Add(new() { Type = currentLogType, Message = line });
+                currentLogType = ShaderCompilerLogEntryType.Warning;
             }
+            else if (line.Contains("error:"))
+            {
+                currentLogType = ShaderCompilerLogEntryType.Error;
+                hasErrors = true;
+            }
+
+            logList.Add(new() { Type = currentLogType, Message = line });
         }
+        
+        while ((line = process.StandardOutput.ReadLine()) != null)
+        {
+            logList.Add(new() { Type = ShaderCompilerLogEntryType.Message, Message = line });
+        }
+        
+        var shaderBytecode = File.ReadAllBytes(outputFilePath);
+
+        File.Delete(inputFilePath);
+        File.Delete(outputFilePath);
 
         return new ShaderCompilerResult
         {
             IsSuccess = !hasErrors,
             LogEntries = logList.ToArray(),
-            ShaderData = shaderData.ToArray()
+            ShaderData = shaderBytecode
         };
     }
 }
-
-internal unsafe readonly struct BinaryBlob : IDxcBlob
-{
-    private readonly void* _unmanagedData;
-    private readonly uint _dataSize;
-
-    public BinaryBlob(ReadOnlySpan<byte> data)
-    {
-        _dataSize = (uint)data.Length;
-
-        _unmanagedData = NativeMemory.Alloc(_dataSize);
-
-        fixed (void* dataPointer = data)
-        {
-            NativeMemory.Copy(dataPointer, _unmanagedData, _dataSize);
-        }
-    }
-
-    // TODO: Disposable and desctructor
-
-    public unsafe char* GetBufferPointer()
-    {
-        return (char*)_unmanagedData;
-    }
-
-    public uint GetBufferSize()
-    {
-        return _dataSize;
-    }
-}
-
-[ComImport]
-[Guid("8BA5FB08-5195-40e2-AC58-0D989C3A0102")]
-[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-internal interface IDxcBlob
-{
-    [PreserveSig]
-    unsafe char* GetBufferPointer();
-    [PreserveSig]
-    UInt32 GetBufferSize();
-}
-
-[ComImport]
-[Guid("8BA5FB08-5195-40e2-AC58-0D989C3A0102")]
-[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-internal interface IDxcBlobEncoding : IDxcBlob
-{
-    System.UInt32 GetEncoding(out bool unknown, out UInt32 codePage);
-}
-
-[ComImport]
-[Guid("CEDB484A-D4E9-445A-B991-CA21CA157DC2")]
-[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-internal interface IDxcOperationResult
-{
-    Int32 GetStatus();
-    IDxcBlob GetResult();
-    IDxcBlobEncoding GetErrors();
-}
-
-[ComImport]
-[Guid("7f61fc7d-950d-467f-b3e3-3c02fb49187c")]
-[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-internal interface IDxcIncludeHandler
-{
-    IDxcBlob LoadSource(string fileName);
-}
-
-[StructLayout(LayoutKind.Sequential)]
-internal struct DXCDefine
-{
-    [MarshalAs(UnmanagedType.LPWStr)]
-    public string pName;
-    [MarshalAs(UnmanagedType.LPWStr)]
-    public string pValue;
-}
-
-[ComImport]
-[Guid("8c210bf3-011f-4422-8d70-6f9acb8db617")]
-[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-internal interface IDxcCompiler
-{
-    IDxcOperationResult Compile(IDxcBlob source, string sourceName, string entryPoint, string targetProfile,
-            [MarshalAs(UnmanagedType.LPArray, ArraySubType =UnmanagedType.LPWStr)]
-            string[] arguments,
-            int argCount, DXCDefine[] defines, int defineCount, IDxcIncludeHandler includeHandler);
-}
-
-internal static partial class DirectXShaderCompilerInterop
-{
-    public static Guid CLSID_DxcCompiler = new("73e22d93-e6ce-47f3-b5bf-f0664f39c1b0");
-
-    [UnmanagedCallConv(CallConvs = new Type[] { typeof(CallConvStdcall) })]
-    [DllImport("ShaderCompilers/dxcompiler")]
-    private static extern int DxcCreateInstance(ref Guid clsid, ref Guid iid, [MarshalAs(UnmanagedType.IUnknown)] out object instance);
-
-    internal static bool DxcCreateInstance<T>(Guid clsid, out T instance)
-    {
-        var guid = typeof(T).GUID;
-        var result = DxcCreateInstance(ref clsid, ref guid, out object outputObject);
-        instance = (T)outputObject;
-        return result == 0;
-    }
-}
-
