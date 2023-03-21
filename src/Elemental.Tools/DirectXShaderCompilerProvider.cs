df07db006fecb2094381269b2e13104fa9e861ca
Thomas Decroyère <thomas.decroyere@outlook.com>
2023-03-21 17:09:29 +0100

    Dxc WIP

--- src/Elemental.Tools/DirectXShaderCompilerProvider.cs
+++ src/Elemental.Tools/DirectXShaderCompilerProvider.cs
@@ -1,6 +1,9 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
 namespace Elemental.Tools;
 
-internal class DirectXShaderCompilerProvider : IShaderCompilerProvider
+internal unsafe class DirectXShaderCompilerProvider : IShaderCompilerProvider
 {
     private readonly string _dxcPath;
 
@@ -29,6 +32,8 @@ internal class DirectXShaderCompilerProvider : IShaderCompilerProvider
 
     public unsafe ShaderCompilerResult CompileShader(ReadOnlySpan<byte> shaderCode, ToolsShaderStage shaderStage, string entryPoint, ShaderLanguage shaderLanguage, ToolsGraphicsApi graphicsApi)
     {
+        TestDxc();
+
         var inputFilePath = $"{Path.GetTempFileName()}.hlsl";
         var outputFilePath = Path.GetTempFileName();
 
@@ -113,5 +118,76 @@ internal class DirectXShaderCompilerProvider : IShaderCompilerProvider
             LogEntries = logList.ToArray(),
             ShaderData = shaderBytecode
         };
+    }
+
+    private void TestDxc()
+    {
+        nint? dxilLibrary = null;
+
+        if (File.Exists("ShaderCompilers/dxil"))
+        {
+            dxilLibrary = NativeLibrary.Load("ShaderCompilers/dxil");
+        }
+
+        var compilerLibrary = NativeLibrary.Load("ShaderCompilers/dxcompiler");
+
+        Guid CLSID_DxcCompiler = new("73e22d93-e6ce-47f3-b5bf-f0664f39c1b0");
+        Guid rfId = typeof(IDxcCompiler3).GUID;
+        nint objectPointer;
+
+        var DxcCreateInstance = (delegate* unmanaged[Stdcall]<Guid*, Guid*, nint*, int>)NativeLibrary.GetExport(compilerLibrary, "DxcCreateInstance");
+        var result = DxcCreateInstance(&CLSID_DxcCompiler, &rfId, &objectPointer);
+
+        //IDxcCompiler3 compiler = Marshal.
+
+        Console.WriteLine($"Result: {result}");
+
+        NativeLibrary.Free(compilerLibrary);
+
+        if (dxilLibrary != null)
+        {
+            NativeLibrary.Free(dxilLibrary.Value);
+        }
+    }
+}
+
+
+[Guid("228B4687-5A6A-4730-900C-9702B2203F54")]
+internal unsafe struct IDxcCompiler3
+{
+    public void** lpVtbl;
+
+    public int QueryInterface(Guid* riid, void** ppvObject)
+    {
+        return ((delegate* unmanaged[Stdcall]<IDxcCompiler3*, Guid*, void**, int>)(lpVtbl[0]))((IDxcCompiler3*)Unsafe.AsPointer(ref this), riid, ppvObject);
+    }
+
+    uint AddRef()
+    {
+        return ((delegate* unmanaged[Stdcall]<IDxcCompiler3*, uint>)(lpVtbl[1]))((IDxcCompiler3*)Unsafe.AsPointer(ref this));
+    }
+
+    public uint Release()
+    {
+        return ((delegate* unmanaged[Stdcall]<IDxcCompiler3*, uint>)(lpVtbl[2]))((IDxcCompiler3*)Unsafe.AsPointer(ref this));
+    }
+/*
+    public int Compile(DxcBuffer* pSource, ushort** pArguments, uint argCount, IDxcIncludeHandler* pIncludeHandler, Guid* riid, void** ppResult)
+    {
+        return ((delegate* unmanaged[Stdcall]<IDxcCompiler3*, DxcBuffer*, ushort**, uint, IDxcIncludeHandler*, Guid*, void**, int>)(lpVtbl[3]))((IDxcCompiler3*)Unsafe.AsPointer(ref this), pSource, pArguments, argCount, pIncludeHandler, riid, ppResult);
+    }
+    
+    public int Disassemble(DxcBuffer* pObject, Guid* riid, void** ppResult)
+    {
+        return ((delegate* unmanaged[Stdcall]<IDxcCompiler3*, DxcBuffer*, Guid*, void**, int>)(lpVtbl[4]))((IDxcCompiler3*)Unsafe.AsPointer(ref this), pObject, riid, ppResult);
+    }*/
+
+    public partial struct Vtbl
+    {
+        public delegate* unmanaged[Stdcall]<IDxcCompiler3*, Guid*, void**, int> QueryInterface;
+        public delegate* unmanaged[Stdcall]<IDxcCompiler3*, uint> AddRef;
+        public delegate* unmanaged[Stdcall]<IDxcCompiler3*, uint> Release;
+        //public delegate* unmanaged[Stdcall]<IDxcCompiler3*, DxcBuffer*, ushort**, uint, IDxcIncludeHandler*, Guid*, void**, int> Compile;
+        //public delegate* unmanaged[Stdcall]<IDxcCompiler3*, DxcBuffer*, Guid*, void**, int> Disassemble;
     }
 }
