76ee911e7a994fa6cce78ffc0e559da52e859dcf
Thomas Decroyère <thomas.decroyere@outlook.com>
2023-05-04 12:03:05 +0200

    Refactor project structure

--- src/Elemental/Inputs/IInputsService.cs
+++ src/Elemental/Inputs/IInputsService.cs
@@ -8,4 +8,46 @@ public interface IInputsService
 {
     // TODO: Allows the user to configure the gamepad deadzone (shape: rectangular, circle, etc and value)
     InputState GetInputState(NativeApplication application);
+
+    /*
+
+    var keyboard = inputService.GetKeyboard();
+
+    if (inputService.IsPressed(keyboard.KeyA))
+    {
+        ...
+    }
+
+    var gamepad = inputService.GetGamepad(0);
+
+    if (inputService.IsPressed(gamepad.Button1))
+    {
+
+    }
+
+    var xMovement = inputService.GetValue(gamepad.LeftStickX);
+
+    var customDevice = inputServcie.GetDevice(...);
+
+    if (inputServcie.IsPressed(customeDevice.InputObject[1]));
+    {
+        ...
+    }
+
+    struct InputObjectState
+    {
+        public float Value;
+        public float PreviousValue;
+        public byte Status; (IsPressed, IsReleased, HasRepeatChanged)
+        public byte RepeatCount;
+        public byte PreviousRepeatCount;
+    }
+    
+    inputService.GetInputObjectState(InputObject.KeyA);
+    inputService.GetInputObjectState(InputObject.Gamepad1Button1);
+
+
+    inputService.GetInputObjectStates(gamepad);
+    */
+
 }
