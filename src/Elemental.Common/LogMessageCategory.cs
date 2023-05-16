a9e9709692cc0d45c99195b54e9234a736fbdaec
Thomas Decroyère <thomas.decroyere@outlook.com>
2023-05-16 06:06:23 +0200

    Add missing files

---
+++ src/Elemental.Common/LogMessageCategory.cs
@@ -0,0 +1,22 @@
+namespace Elemental;
+
+/// <summary>
+/// Categories of log messages.
+/// </summary>
+public enum LogMessageCategory
+{
+    /// <summary>
+    /// Native application log message category.
+    /// </summary>
+    NativeApplication,
+    
+    /// <summary>
+    /// Graphics log message category.
+    /// </summary>
+    Graphics,
+    
+    /// <summary>
+    /// Inputs log message category.
+    /// </summary>
+    Inputs
+}
---
+++ src/Elemental.Common/LogMessageType.cs
@@ -0,0 +1,22 @@
+namespace Elemental;
+
+/// <summary>
+/// Type of log message.
+/// </summary>
+public enum LogMessageType
+{
+    /// <summary>
+    /// Debug log message type.
+    /// </summary>
+    Debug = 0,
+
+    /// <summary>
+    /// Warning log message type.
+    /// </summary>
+    Warning = 1,
+
+    /// <summary>
+    /// Error log message type.
+    /// </summary>
+    Error = 2
+}
