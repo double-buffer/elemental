809f1d120a30cf481caf929c4a0c64393afaede9
Thomas Decroyère <thomas.decroyere@outlook.com>
2023-12-19 05:36:40 +0100

    Fix Issues

--- src/Elemental.Native/Windows/SystemPlatformFunctions.cpp
+++ src/Elemental.Native/Windows/SystemPlatformFunctions.cpp
@@ -33,7 +33,7 @@ void* SystemPlatformAllocateMemory(size_t sizeInBytes)
     return HeapAlloc(GetProcessHeap(), 0, sizeInBytes);
 }
 
-void SystemPlatformFreeMemory(void* pointer)
+void SystemPlatformFreeMemory(void* pointer, size_t sizeInBytes)
 {
     HeapFree(GetProcessHeap(), 0, pointer);
 }
