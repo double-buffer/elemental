2108aa103a64d5e60a54e70108d79cabe1667945
Thomas Decroyère <thomas.decroyere@outlook.com>
2023-12-19 09:18:32 +0100

    Add missing files

---
+++ tests/NativeSystemFunctionsTests/UnityBuild.cpp
@@ -0,0 +1,14 @@
+#include "SystemPlatformFunctions.cpp"
+#include "SystemLogging.cpp"
+#include "SystemMemory.cpp"
+#include "SystemFunctions.cpp"
+#include "SystemDictionary.cpp"
+#include "SystemInputs.cpp"
+
+#include "utest.h"
+
+#include "IOTests.cpp"
+#include "MathTests.cpp"
+#include "StringTests.cpp"
+
+UTEST_MAIN();
