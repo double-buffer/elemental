98ff96fafaa103c3eef2acbc20007eea6a154fe6
Thomas Decroyère <thomas.decroyere@outlook.com>
2024-01-29 05:56:07 +0100

    Fist attempt with C sample

---
+++ samples/c/01-HelloWorld/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include "Elemental.h"
+
+int32_t counter = 0;
+
+// TODO: Provide a default handler that can be used out of the box for other samples. We show how to provide one here on the first one
+void LogMessageHandler(LogMessageType messageType, LogMessageCategory category, ReadOnlySpan<char> function, ReadOnlySpan<char> message) 
+{
+    printf("[");
+    printf("\033[36m");
+
+    // TODO: Provide a mapping function
+    if (category == LogMessageCategory_Memory)
+    {
+        printf("Memory");
+    }
+    else if (category == LogMessageCategory_NativeApplication)
+    {
+        printf("Application");
+    }
+    else if (category == LogMessageCategory_Graphics)
+    {
+        printf("Graphics");
+    }
+    else if (category == LogMessageCategory_Inputs)
+    {
+        printf("Inputs");
+    }
+
+    printf("\033[0m]");
+
+    printf("\033[32m %s", function.Pointer);
+
+    if (messageType == LogMessageType_Error)
+    {
+        printf("\033[31m");
+    }
+    else if (messageType == LogMessageType_Warning)
+    {
+        printf("\033[33m");
+    }
+    else
+    {
+        printf("\033[0m");
+    }
+
+    printf(" %s\n\033[0m", message.Pointer);
+}
+
+bool RunHandler(elemApplicationStatus status)
+{
+    if (counter > 10 || !status.IsActive)
+    {
+        return false;
+    }
+
+    printf("Hello World {counter}!", counter++);
+    return true;
+}
+
+void main()
+{
+    auto applicationService = elemInitApplicationService({ LogMessageHandler = LogMessageHandler });
+    auto application = elemCreateApplication(applicationService, "Hello World");
+
+    elemRunApplication(application, RunHandler);
+
+    elemFreeApplication(application);
+    elemFreeApplicationService(applicationService);
+}
