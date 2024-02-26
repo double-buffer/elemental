d6edd09c69bda183c3e1e7c02974fbc22d51fce0
Thomas Decroyère <thomas.decroyere@outlook.com>
2024-02-26 15:03:25 +0100

    Finish implementation of DataPool

--- src/Elemental/Common/SystemDataPool.cpp
+++ src/Elemental/Common/SystemDataPool.cpp
@@ -12,13 +12,15 @@ struct SystemDataPoolStorageItem
     uint32_t Next;
 };
 
-template<typename T>
+template<typename T, typename TFull>
 struct SystemDataPoolStorage
 {
     MemoryArena MemoryArena;
     Span<SystemDataPoolStorageItem<T>> Data;
+    Span<TFull> DataFull;
     uint32_t CurrentIndex;
     uint32_t FreeListIndex;
+    uint32_t ItemCount;
 };
 
 struct SystemDataPoolHandle
@@ -42,32 +44,47 @@ SystemDataPoolHandle UnpackSystemDataPoolHandle(uint64_t packedValue)
 }
 
 template<typename T>
-SystemDataPool<T> SystemCreateDataPool(MemoryArena memoryArena, size_t maxItems)
+bool IsTypeEmpty()
 {
-    auto storage = SystemPushStructZero<SystemDataPoolStorage<T>>(memoryArena);
+    return sizeof(T) == 1;
+}
+
+template<typename T, typename TFull>
+SystemDataPool<T, TFull> SystemCreateDataPool(MemoryArena memoryArena, size_t maxItems)
+{
+    auto storage = SystemPushStructZero<SystemDataPoolStorage<T, TFull>>(memoryArena);
     storage->MemoryArena = memoryArena;
     storage->Data = SystemPushArray<SystemDataPoolStorageItem<T>>(memoryArena, maxItems, AllocationState_Reserved);
+    
+    if (!IsTypeEmpty<TFull>())
+    {
+        storage->DataFull = SystemPushArray<TFull>(memoryArena, maxItems, AllocationState_Reserved);
+    }
+    
     storage->FreeListIndex = SYSTEM_DATAPOOL_INDEX_EMPTY;
 
-    SystemDataPool<T> result = {};
+    SystemDataPool<T, TFull> result = {};
     result.Storage = storage;
     return result;
 }
 
-template<typename T>
-ElemHandle SystemAddDataPoolItem(SystemDataPool<T> dataPool, T data)
+template<typename T, typename TFull>
+ElemHandle SystemAddDataPoolItem(SystemDataPool<T, TFull> dataPool, T data)
 {
     SystemDataPoolHandle result = {};
     auto storage = dataPool.Storage;
-
-    // TODO: Atomics!
     auto index = SYSTEM_DATAPOOL_INDEX_EMPTY;
 
-    if (storage->FreeListIndex != SYSTEM_DATAPOOL_INDEX_EMPTY)
+    do
     {
+        if (storage->FreeListIndex == SYSTEM_DATAPOOL_INDEX_EMPTY)
+        {
+            index = SYSTEM_DATAPOOL_INDEX_EMPTY;
+            break;
+        }
+        
         index = storage->FreeListIndex;
-        storage->FreeListIndex = storage->Data[storage->FreeListIndex].Next;
-    }
+    } while (!SystemAtomicCompareExchange(storage->FreeListIndex, index, storage->Data[storage->FreeListIndex].Next));
 
     if (index == SYSTEM_DATAPOOL_INDEX_EMPTY)
     {
@@ -80,35 +97,61 @@ ElemHandle SystemAddDataPoolItem(SystemDataPool<T> dataPool, T data)
 
         index = SystemAtomicAdd(storage->CurrentIndex, 1);
         SystemCommitMemory<SystemDataPoolStorageItem<T>>(storage->MemoryArena, storage->Data.Slice(index), true);
+        
+        if (!IsTypeEmpty<TFull>())
+        {
+            SystemCommitMemory<TFull>(storage->MemoryArena, storage->DataFull.Slice(index), true);
+        }
     }
 
     storage->Data[index].Data = data;
     storage->Data[index].Next = SYSTEM_DATAPOOL_INDEX_EMPTY;
+        
+    SystemAtomicAdd(storage->ItemCount, 1);
 
     result.Index = index;
     result.Version = storage->Data[index].Version;
     return PackSystemDataPoolHandle(result);
 }
     
-template<typename T>
-void SystemRemoveDataPoolItem(SystemDataPool<T> dataPool, ElemHandle handle)
+template<typename T, typename TFull>
+void SystemAddDataPoolItemFull(SystemDataPool<T, TFull> dataPool, ElemHandle handle, TFull data)
+{
+    if (IsTypeEmpty<TFull>())
+    {
+        SystemLogErrorMessage(ElemLogMessageCategory_Memory, "Cannot add full data because data pool was not created with a full type.");
+        return;
+    }
+    
+    auto storage = dataPool.Storage;
+    auto dataPoolHandle = UnpackSystemDataPoolHandle(handle);
+
+    storage->DataFull[dataPoolHandle.Index] = data;
+}
+
+template<typename T, typename TFull>
+void SystemRemoveDataPoolItem(SystemDataPool<T, TFull> dataPool, ElemHandle handle)
 {
     auto storage = dataPool.Storage;
     auto dataPoolHandle = UnpackSystemDataPoolHandle(handle);
 
-    // TODO: Atomic!
-
-    if (dataPoolHandle.Version == storage->Data[dataPoolHandle.Index].Version)
+    if (dataPoolHandle.Version != storage->Data[dataPoolHandle.Index].Version)
     {
-        storage->Data[dataPoolHandle.Index].Version++;
+        SystemLogWarningMessage(ElemLogMessageCategory_Memory, "Trying to remove an already deleted handle.");
+        return;
     }
 
-    storage->Data[dataPoolHandle.Index].Next = storage->FreeListIndex;
-    storage->FreeListIndex = dataPoolHandle.Index;
+    storage->Data[dataPoolHandle.Index].Version = SystemAtomicAdd(storage->Data[dataPoolHandle.Index].Version, 1) + 1;
+    SystemAtomicSubstract(storage->ItemCount, 1);
+
+    do
+    {
+        storage->Data[dataPoolHandle.Index].Next = storage->FreeListIndex;
+    } while (!SystemAtomicCompareExchange(storage->FreeListIndex, storage->FreeListIndex, dataPoolHandle.Index));
 }
 
-template<typename T>
-T* SystemGetDataPoolItem(SystemDataPool<T> dataPool, ElemHandle handle)
+template<typename T, typename TFull>
+T* SystemGetDataPoolItem(SystemDataPool<T, TFull> dataPool, ElemHandle handle)
 {
     auto storage = dataPool.Storage;
     auto dataPoolHandle = UnpackSystemDataPoolHandle(handle);
@@ -121,4 +164,26 @@ T* SystemGetDataPoolItem(SystemDataPool<T> dataPool, ElemHandle handle)
     }
 
     return result;
+}
+
+template<typename T, typename TFull>
+TFull* SystemGetDataPoolItemFull(SystemDataPool<T, TFull> dataPool, ElemHandle handle)
+{
+    auto storage = dataPool.Storage;
+    auto dataPoolHandle = UnpackSystemDataPoolHandle(handle);
+    
+    TFull* result = nullptr;   
+
+    if (dataPoolHandle.Version != SYSTEM_DATAPOOL_INDEX_EMPTY && storage->CurrentIndex > dataPoolHandle.Index && storage->Data[dataPoolHandle.Index].Version == dataPoolHandle.Version)
+    {
+        result = &storage->DataFull[dataPoolHandle.Index];
+    }
+
+    return result;
+}
+
+template<typename T, typename TFull>
+size_t SystemGetDataPoolItemCount(SystemDataPool<T, TFull> dataPool)
+{
+    return dataPool.Storage->ItemCount;
 }
--- tests/SystemFunctionsTests/DataPoolTests.cpp
+++ tests/SystemFunctionsTests/DataPoolTests.cpp
@@ -1,4 +1,5 @@
 #include "SystemDataPool.h"
+#include "SystemFunctions.h"
 #include "utest.h"
 
 struct DataPoolTestData
@@ -13,6 +14,48 @@ struct DataPoolTestDataFull
     uint64_t Data3;
     uint64_t Data4;
 };
+
+struct DataPoolThreadParameter
+{
+    SystemDataPool<DataPoolTestData, DataPoolTestDataFull> DataPool;
+    int32_t ThreadId;
+    int32_t ItemCount;
+    Span<ElemHandle> Handles;
+};
+
+void DataPoolConcurrentAddFunction(void* parameter)
+{
+    auto threadParameter = (DataPoolThreadParameter*)parameter;
+    auto dataPool = threadParameter->DataPool;
+
+    for (int32_t i = 0; i < threadParameter->ItemCount; i++)
+    {
+        DataPoolTestData testData = {};
+        testData.Data = threadParameter->ThreadId * threadParameter->ItemCount + i;
+
+        DataPoolTestDataFull testDataFull = {};
+        testDataFull.Data1 = threadParameter->ThreadId * threadParameter->ItemCount + i;
+        testDataFull.Data2 = threadParameter->ThreadId * threadParameter->ItemCount + i + 1;
+        testDataFull.Data3 = threadParameter->ThreadId * threadParameter->ItemCount + i + 2;
+        testDataFull.Data4 = threadParameter->ThreadId * threadParameter->ItemCount + i + 3;
+
+        auto handle = SystemAddDataPoolItem(dataPool, testData);
+        SystemAddDataPoolItemFull(dataPool, handle, testDataFull);
+
+        threadParameter->Handles[i] = handle;
+    }
+}
+
+void DataPoolConcurrentRemoveFunction(void* parameter)
+{
+    auto threadParameter = (DataPoolThreadParameter*)parameter;
+    auto dataPool = threadParameter->DataPool;
+
+    for (int32_t i = 0; i < threadParameter->ItemCount; i++)
+    {
+        SystemRemoveDataPoolItem(dataPool, threadParameter->Handles[i]);
+    }
+}
 
 UTEST(DataPool, AddItem) 
 {
@@ -116,8 +159,130 @@ UTEST(DataPool, AddItemWithFull)
     ASSERT_EQ(testData.Data, result->Data);
 
     auto resultFull = SystemGetDataPoolItemFull(dataPool, handle);
-    ASSERT_EQ(testDataFull.Data1, result->Data1);
-    ASSERT_EQ(testDataFull.Data2, result->Data2);
-    ASSERT_EQ(testDataFull.Data3, result->Data3);
-    ASSERT_EQ(testDataFull.Data4, result->Data4);
+    ASSERT_EQ(testDataFull.Data1, resultFull->Data1);
+    ASSERT_EQ(testDataFull.Data2, resultFull->Data2);
+    ASSERT_EQ(testDataFull.Data3, resultFull->Data3);
+    ASSERT_EQ(testDataFull.Data4, resultFull->Data4);
+}
+
+UTEST(DataPool, RemoveItemWithFull) 
+{
+    // Arrange
+    auto stackMemoryArena = SystemGetStackMemoryArena();
+    auto dataPool = SystemCreateDataPool<DataPoolTestData, DataPoolTestDataFull>(stackMemoryArena, 10);
+
+    DataPoolTestData testData = {};
+    testData.Data = 5;
+
+    DataPoolTestDataFull testDataFull = {};
+    testDataFull.Data1 = 5;
+    testDataFull.Data2 = 6;
+    testDataFull.Data3 = 7;
+    testDataFull.Data4 = 8;
+
+    auto handle = SystemAddDataPoolItem(dataPool, testData);
+    SystemAddDataPoolItemFull(dataPool, handle, testDataFull);
+
+    // Act
+    SystemRemoveDataPoolItem(dataPool, handle);
+
+    // Assert
+    auto result = SystemGetDataPoolItemFull(dataPool, handle);
+    ASSERT_TRUE(result == nullptr);
+}
+
+UTEST(DataPool, ConcurrentAdd) 
+{
+    // Arrange
+    const int32_t itemCount = 80000;
+    const int32_t threadCount = 32;
+    auto stackMemoryArena = SystemGetStackMemoryArena();
+    auto dataPool = SystemCreateDataPool<DataPoolTestData, DataPoolTestDataFull>(stackMemoryArena, itemCount);
+    
+    // Act
+    SystemThread threads[threadCount];
+    DataPoolThreadParameter threadParameters[threadCount];
+
+    for (int32_t i = 0; i < threadCount; i++)
+    {
+        threadParameters[i] = { dataPool, i, itemCount / threadCount };
+        threadParameters[i].Handles = SystemPushArray<ElemHandle>(stackMemoryArena, threadParameters[i].ItemCount);
+        threads[i] = SystemCreateThread(DataPoolConcurrentAddFunction, &threadParameters[i]);
+    }
+
+    for (int32_t i = 0; i < threadCount; i++)
+    {
+        SystemWaitThread(threads[i]);
+        SystemFreeThread(threads[i]);
+    }
+
+    // Assert
+    auto dataPoolCount = SystemGetDataPoolItemCount(dataPool);
+    ASSERT_EQ(itemCount, dataPoolCount);
+
+    for (int32_t i = 0; i < threadCount; i++)
+    {
+        auto threadParameter = threadParameters[i];
+
+        for (int32_t j = 0; j < itemCount / threadCount; j++)
+        {
+            auto testData = SystemGetDataPoolItem(dataPool, threadParameter.Handles[j]);
+            ASSERT_EQ(threadParameter.ThreadId * threadParameter.ItemCount + j, testData->Data);
+
+            auto testDataFull = SystemGetDataPoolItemFull(dataPool, threadParameter.Handles[j]);
+            ASSERT_EQ(threadParameter.ThreadId * threadParameter.ItemCount + j, testDataFull->Data1);
+            ASSERT_EQ(threadParameter.ThreadId * threadParameter.ItemCount + j + 1, testDataFull->Data2);
+            ASSERT_EQ(threadParameter.ThreadId * threadParameter.ItemCount + j + 2, testDataFull->Data3);
+            ASSERT_EQ(threadParameter.ThreadId * threadParameter.ItemCount + j + 3, testDataFull->Data4);
+        }
+    }
+}
+
+UTEST(DataPool, ConcurrentRemove) 
+{
+    // Arrange
+    const int32_t itemCount = 80000;
+    const int32_t threadCount = 32;
+    auto stackMemoryArena = SystemGetStackMemoryArena();
+    auto dataPool = SystemCreateDataPool<DataPoolTestData, DataPoolTestDataFull>(stackMemoryArena, itemCount);
+    
+    // Act
+    SystemThread threads[threadCount];
+    SystemThread threads2[threadCount];
+    DataPoolThreadParameter threadParameters[threadCount];
+    DataPoolThreadParameter threadParameters2[threadCount];
+
+    for (int32_t i = 0; i < threadCount; i++)
+    {
+        threadParameters[i] = { dataPool, i, itemCount / 2 / threadCount };
+        threadParameters[i].Handles = SystemPushArray<ElemHandle>(stackMemoryArena, threadParameters[i].ItemCount);
+        threads[i] = SystemCreateThread(DataPoolConcurrentAddFunction, &threadParameters[i]);
+    }
+
+    for (int32_t i = 0; i < threadCount; i++)
+    {
+        SystemWaitThread(threads[i]);
+        SystemFreeThread(threads[i]);
+    }
+
+    for (int32_t i = 0; i < threadCount; i++)
+    {
+        threadParameters2[i] = { dataPool, i, itemCount / 2 / threadCount };
+        threadParameters2[i].Handles = SystemPushArray<ElemHandle>(stackMemoryArena, threadParameters2[i].ItemCount);
+        threads2[i] = SystemCreateThread(DataPoolConcurrentAddFunction, &threadParameters2[i]);
+
+        threads[i] = SystemCreateThread(DataPoolConcurrentRemoveFunction, &threadParameters[i]);
+    }
+
+    for (int32_t i = 0; i < threadCount; i++)
+    {
+        SystemWaitThread(threads[i]);
+        SystemWaitThread(threads2[i]);
+        SystemFreeThread(threads[i]);
+        SystemFreeThread(threads2[i]);
+    }
+
+    // Assert
+    auto dataPoolCount = SystemGetDataPoolItemCount(dataPool);
+    ASSERT_EQ(itemCount / 2, dataPoolCount);
 }
