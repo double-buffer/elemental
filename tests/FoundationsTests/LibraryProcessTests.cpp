9527cfde151180fa1340320b4fe0f25316333c7f
Thomas Decroyère <thomas.decroyere@outlook.com>
2026-09-06 10:21:24 +0200

    Add assertion messages to process tests

--- tests/FoundationsTests/LibraryProcessTests.cpp
+++ tests/FoundationsTests/LibraryProcessTests.cpp
@@ -1,14 +1,14 @@
 #include "SystemFunctions.h"
 #include "utest.h"
 
-UTEST(LibraryProcess, SystemExecuteProcess) 
+UTEST(LibraryProcess, SystemExecuteProcess)
 {
     // Arrange
     auto stackMemoryArena = SystemGetStackMemoryArena();
-    
+
     // Act
     auto result = SystemExecuteProcess(stackMemoryArena, "ping");
 
     // Assert
-    ASSERT_GT((int32_t)result.Length, 0);
+    ASSERT_GT_MSG((int32_t)result.Length, 0, "SystemExecuteProcess should return non-empty process output.");
 }
